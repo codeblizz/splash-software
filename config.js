f8034f2f27f9d0b4970aa5f481492e56ea3c0cd0
Collins Maduka <codeblizz@Codeblizz.lan>
2022-08-21 00:21:43 +0400

    added heroku build setup

---
+++ config.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+
+const { PORT } = dotenv.config({ path: __dirname + "/.env"}).parsed;
+
+const config = {
+    PORT
+}
+
+module.exports = config;
--- server/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-
-const { PORT } = dotenv.config({ path: __dirname + "/../.env"}).parsed;
-
-const config = {
-    PORT
-}
-
-module.exports = config;
